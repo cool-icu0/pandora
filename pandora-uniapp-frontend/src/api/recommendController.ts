7be65a9854080c1896043cf4bd215d45d59498c2
Cool <990299103@qq.com>
2025-05-18 18:43:10 +0800

    feat(api): 添加推荐管理相关接口
    - 新增问题推荐列表接口
    - 新增问题推荐状态更新接口
    - 新增用户推荐列表接口
    - 新增用户推荐状态更新接口

---
+++ pandora-uniapp-frontend/src/api/recommendController.ts
@@ -0,0 +1,69 @@
+// @ts-ignore
+/* eslint-disable */
+import request from "@/utils/request";
+
+/** getQuestionRecommendList POST /api/recommend/question/list */
+export async function getQuestionRecommendListUsingPost(
+  body: API.QuestionRecommendRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageQuestionRecommendVO_>(
+    "/api/recommend/question/list",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: body,
+      ...(options || {}),
+    }
+  );
+}
+
+/** updateQuestionRecommendStatus POST /api/recommend/question/status */
+export async function updateQuestionRecommendStatusUsingPost(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.updateQuestionRecommendStatusUsingPOSTParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponse>("/api/recommend/question/status", {
+    method: "POST",
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** getUserRecommendList POST /api/recommend/user/list */
+export async function getUserRecommendListUsingPost(
+  body: API.UserRecommendRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageUserRecommendVO_>(
+    "/api/recommend/user/list",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: body,
+      ...(options || {}),
+    }
+  );
+}
+
+/** updateUserRecommendStatus POST /api/recommend/user/status */
+export async function updateUserRecommendStatusUsingPost(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.updateUserRecommendStatusUsingPOSTParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponse>("/api/recommend/user/status", {
+    method: "POST",
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
